c463977c3ba06071d823a37a797c3add4cbfae5f
Lianchangle <lsiricreat@163.com>
2024-02-04 16:34:30 +0800

    no message

--- TestPythonInstall/print_profiler_info.py
+++ TestPythonInstall/print_profiler_info.py
@@ -3,23 +3,36 @@ from mecheye.shared import *
 from mecheye.profiler import *
 from mecheye.profiler_utils import *
 
+def print_profiler_info1(profiler_info: ProfilerInfo):
+    print(".........................................")
+    print("Profiler Model Name:           ", profiler_info.model)
+    print("Controller Serial Number:      ", profiler_info.controller_sn)
+    print("Sensor Serial Number:          ", profiler_info.sensor_sn)
+    print("Profiler IP Address:           ", profiler_info.ip_address)
+    print("Profiler IP Subnet Mask:       ", profiler_info.subnet_mask)
+    print("Profiler IP Assignment Method: ", ip_assignment_method_to_string(profiler_info.ip_assignment_method))
+    print("Hardware Version:              V", profiler_info.hardware_version.to_string())
+    print("Firmware Version:              V", profiler_info.firmware_version.to_string())
+    print(".........................................")
+    print()
+
 
 class PrintProfilerInfo(object):
     def __init__(self, ip):
         self.ip = ip
-        self.camera = Profiler()
-        self.camera_info = ProfilerInfo()
+        self.profiler = Profiler()
+        self.profiler_info = ProfilerInfo()
 
     def print_device_info(self):
-        show_error(self.camera.get_profiler_info(self.camera_info))
-        print_profiler_info(self.camera_info)
+        show_error(self.profiler.get_profiler_info(self.profiler_info))
+        print_profiler_info1(self.profiler_info)
 
 
     def main(self):
-        self.camera.connect(self.ip)    
+        self.profiler.connect(self.ip)    
         self.print_device_info()
-        self.camera.disconnect()
-        print("Disconnected from the camera successfully.")
+        self.profiler.disconnect()
+        print("Disconnected from the profiler successfully.")
 
 
 
